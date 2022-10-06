8e15d29d3624a3fe66c181c2964cfae5d39fedc8
muckas <motobike8@outlook.com>
2022-10-06 20:27:17 +0500

    Reading keyboard input events

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <linux/input.h>
 
 int main() {
-	printf("Test\n");
-	return 0;
+	char device_path[] = "/srv/input/kbd0";
+	int device = open(device_path, O_RDONLY);
+	struct input_event event;
+	while(1) {
+		read(device, &event, sizeof(event));
+		if (event.type == 1 && event.value == 1) {
+			printf("Key: %i State: %i\n", event.code, event.value);
+		}
+	}
 }
