fe2c9330003ddf201fce8147e2e266ce796d6b13
muckas <motobike8@outlook.com>
2022-10-07 13:28:06 +0500

    Command line arguments

--- main.c
+++ main.c
@@ -1,13 +1,69 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <linux/input.h>
 #include <linux/input-event-codes.h>
 #include <sys/time.h>
-#include <errno.h>
+#include <argp.h>
 
-void inject_event(int device, __u16 code, __s32 value) {
+const char *argp_program_version = "mouse-emulator 0.1.0";
+static char doc[] = "Emulates mouse actions with keyboard keys\n Keycodes defined in linux/input-event-codes.h";
+static char args_doc[] = "--input [FILE] --output [FILE]";
+static struct argp_option options[] = {
+	{"input", 'i', "Input", 0, "Input device (keyboard) path"}, 
+	{"output", 'o', "Output", 0, "Output device (mouse) path"}, 
+	{"mod", 'M', "ModKey", OPTION_ARG_OPTIONAL, "Keyboard key code for modifier key"}, 
+	{"mouseleft", 'l', "MouseLeft", OPTION_ARG_OPTIONAL, "Keyboard key code for LMB"}, 
+	{"mouselefthold", 'L', "MouseLeftHold", OPTION_ARG_OPTIONAL, "Keyboard key code for holding LMB"}, 
+	{"mouseright", 'r', "MouseRight", OPTION_ARG_OPTIONAL, "Keyboard key code for RMB"}, 
+	{"mousemiddle", 'm', "MouseMiddle", OPTION_ARG_OPTIONAL, "Keyboard key code for MMB"}, 
+};
+
+struct arguments{
+	char *Input;
+	char *Output;
+	int Mod;
+	int MouseLeft;
+	int MouseLeftHold;
+	int MouseRight;
+	int MouseMiddle;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+	struct arguments *arguments = state->input;
+	switch(key){
+		case 'i':
+			arguments->Input = arg;
+			break;
+		case 'o':
+			arguments->Output = arg;
+			break;
+		case 'M':
+			arguments->Mod = atoi(arg);
+			break;
+		case 'l':
+			arguments->MouseLeft = atoi(arg);
+			break;
+		case 'L':
+			arguments->MouseLeftHold = atoi(arg);
+			break;
+		case 'r':
+			arguments->MouseRight = atoi(arg);
+			break;
+		case 'm':
+			arguments->MouseMiddle = atoi(arg);
+			break;
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+struct argp argp = {options, parse_opt, args_doc, doc};
+
+void inject_event(int device, int code, int value) {
 	struct input_event event_key, event_end;
 
 	gettimeofday(&event_key.time, NULL);
@@ -23,58 +79,66 @@ void inject_event(int device, __u16 code, __s32 value) {
 	write(device, &event_end, sizeof(event_end));
 }
 
-void mouse_click(int device, __u16 code) {
+void mouse_click(int device, int code) {
 	inject_event(device, code, 1);
 	inject_event(device, code, 0);
 }
 
-void mouse_down(int device, __u16 code) {
+void mouse_down(int device, int code) {
 	inject_event(device, code, 1);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+	struct arguments arguments;
+
+	arguments.Input = "";
+	arguments.Output = "";
+	arguments.Mod = 0;
+	arguments.MouseLeft = 0;
+	arguments.MouseLeftHold = 0;
+	arguments.MouseRight = 0;
+	arguments.MouseMiddle = 0;
+
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
 	bool modkey_down = false;
-	char kbd_path[] = "/srv/input/kbd0";
-	char mouse_path[] = "/srv/input/mouse0";
-	int kbd = open(kbd_path, O_RDONLY | O_NONBLOCK);
-	int mouse = open(mouse_path, O_RDWR);
-	if (kbd < 0 || mouse < 0) {
-		printf("Error opening device: %i\n", errno);
-		return -1;
+	int kbd = open(arguments.Input, O_RDONLY | O_NONBLOCK);
+	if (kbd < 0) {
+		printf("Error opening input device %s\n", arguments.Input);
+		exit(EXIT_FAILURE);
+	}
+	int mouse = open(arguments.Output, O_RDWR);
+	if (mouse < 0) {
+		printf("Error opening output device %s\n", arguments.Output);
+		exit(EXIT_FAILURE);
 	}
 	struct input_event event;
 	while(1) {
 		read(kbd, &event, sizeof(event));
 		if (event.type == EV_KEY) {
-			switch (event.code) {
-				case KEY_CAPSLOCK:
-					if (event.value == 1) {
-						modkey_down = true;
-					} 
-					if (event.value == 0) {
-						modkey_down = false;
-					}
-					break;
-				case KEY_7: // mouse left
-					if (event.value == 0) {
-						mouse_click(mouse, 272);
-					}
-					break;
-				case KEY_8: // mouse right
-					if (event.value == 0) {
-						mouse_click(mouse, 273);
-					}
-					break;
-				case KEY_Y: // mouse middle
-					if (event.value == 0) {
-						mouse_click(mouse, 274);
-					}
-					break;
-				case KEY_U: // hold left mouse
-					if (event.value == 0) {
-						mouse_down(mouse, 272);
-					}
-					break;
+			if (event.code == arguments.Mod) {
+				if (event.value == 1) {
+					modkey_down = true;
+				} 
+				if (event.value == 0) {
+					modkey_down = false;
+				}
+			} else if (event.code == arguments.MouseLeft) {
+				if (event.value == 0) {
+					mouse_click(mouse, 272);
+				}
+			} else if (event.code == arguments.MouseLeftHold) {
+				if (event.value == 0) {
+					mouse_down(mouse, 272);
+				}
+			} else if (event.code == arguments.MouseRight) {
+				if (event.value == 0) {
+					mouse_click(mouse, 273);
+				}
+			} else if (event.code == arguments.MouseMiddle) {
+				if (event.value == 0) {
+					mouse_click(mouse, 274);
+				}
 			}
 		}
 	}
